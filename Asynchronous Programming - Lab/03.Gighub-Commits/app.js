30574594bf4b43ae2f26993f55f69e4ed9217955
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-12-01 20:22:18 +0200

    03. All done 100/100 :)

--- Asynchronous Programming - Lab/03.Gighub-Commits/app.js
+++ Asynchronous Programming - Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,45 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+
+    fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+        .then(handleResponse)
+        .then(handleData)
+        .catch(handleError);
+
+    function handleResponse(response) {
+        //console.log(response);
+        if (response.ok == false) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    }
+    function handleData(data) {
+        //console.log(data);
+
+        const list = document.getElementById('commits');
+        // list.replaceChildren();
+        // for (let item of data) {
+        //     const li = document.createElement('li');
+        //     li.textContent = `${item.commit.author.name}: ${item.commit.message}`;
+        //     list.appendChild(li);
+
+        // }
+        const items = data.map(item=>{
+            const li = document.createElement('li');
+            li.textContent = `${item.commit.author.name}: ${item.commit.message}`;
+            return li;
+        });
+        //console.log(items);
+        list.replaceChildren(...items);
+
+    }
+    function handleError(error) {
+        const list = document.getElementById('commits');
+        const li = document.createElement('li');
+        li.textContent = `Error: ${error.message} (Not Found)`;
+
+        list.replaceChildren(li);
+
+    }
 }
