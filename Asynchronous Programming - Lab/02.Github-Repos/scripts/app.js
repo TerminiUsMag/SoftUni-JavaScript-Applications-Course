c4abf56c9253f65255c54ed6c2250ec1ac02afe2
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-12-01 19:38:28 +0200

    02. All done 100/100 :)

--- Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
+++ Asynchronous Programming - Lab/02.Github-Repos/scripts/app.js
@@ -7,18 +7,40 @@ function loadRepos() {
 		.catch(handleError);
 
 	function handleResponse(response) {
-		console.log(response);
-		if (response.ok === false) {
-			throw new Error(response.status);
+		//console.log(response);
+		if (response.ok == false) {
+			throw new Error(response.status + ' '+ response.statusText);
 		}
-		response.json();
+		return response.json();
 	}
 
 	function handleData(data) {
+		//console.log(data);
+
+		// <li>
+		// 	<a href="{repo.html_url}">
+		// 		{repo.full_name}
+		// 	</a>
+		// </li>
+		const ul = document.getElementById('repos');
+		const items = data.map(item => {
+			const li = document.createElement('li');
+			const a = document.createElement('a');
+			a.setAttribute('href', item.html_url);
+			a.textContent = `{${item.full_name}}`;
+			li.appendChild(a);
+			// ul.appendChild(li);
+			return li;
+
+		});
+
+		ul.replaceChildren(...items);
 
 	}
 
 	function handleError(error) {
+		const list = document.getElementById('repos');
+		list.textContent = error.message;
 
 	}
 }
